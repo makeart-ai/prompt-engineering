724cd67fe1427155c79235f25aab6f14ceb1b4e5
Adam Schmideg <adamschmideg@users.noreply.github.com>
2022-10-30 13:39:49 +0100

    Initial set up to use api key

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import openai
+
+def setApiKey():
+    try:
+        import secretenvvars
+        openai.api_key = secretenvvars.openai_api_key
+    except ImportError:
+        openai.api_key = os.environ.get("OPENAI_API_KEY")
+    if not openai.api_key:
+        print("API key not found")
+        return False
+    return True
+
+def main():
+    if not setApiKey():
+        return
+    completion = openai.Completion.create(engine="ada", prompt="Hello world")
+    print(completion.choices[0].text)
+
+
+if __name__ == "__main__":
+    main()
