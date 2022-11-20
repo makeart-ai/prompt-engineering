16d4b7c2c76a2f1e03e6124e7ba476e481ade1d9
Adam Schmideg <adamschmideg@users.noreply.github.com>
2022-11-20 13:30:24 +0100

    The woman who cried milk

---
+++ playground/cried_milk.py
@@ -0,0 +1,49 @@
+# A story of a girl, a fridge and a cart
+# The (Wo)man who cried milk
+# The milk that cried wolf
+
+with open(fridge, by=sarah):
+    sarah.smells(milk)
+    assert milk.best_before <= datetime.now()
+    milk.age == time(days=2)
+    if time and again:
+        food = sarah.select() and food is not milk
+
+with sarah.make_note(type='mental') as grocery_list:
+    if sarah in grocery_store:
+        cart.add(milk)
+        cart.add(milk) if milk.fat_content > 0.03
+
+if sarah in os.HOME:
+    try:
+        milk.discard() if milk.best_before > datetime.now()
+        import milk from shopping_bag
+        open(milk)
+    except OverflowError:
+        raise Hands()
+    except MemoryError:
+        music.query(smells, 'like', '%teen spirit%')
+        break
+
+    if sarah.last_time < 10:
+        music.query(artist, 'like', '%teen spirit%')
+        dance.shuffle()
+        sleep(1)
+    for dance in rooms:
+        continue
+
+    sarah.teeth.brush()
+    sarah.wash()
+    sarah.groom()
+    sarah.make_up()
+    sarah.style()
+    sarah.perfume()
+    sarah.dress()
+    sarah.shoes()
+    sarah.accessories()
+    sarah.fragrance()
+    sarah.jewelry()
+    sarah.look()
+
+    for dance in rooms:
+        continue
